9954a202dee18606971db4285c2cda60c5aedf7e
DMAN <ranjan.dr29@gmail.com>
2024-03-31 18:39:49 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,AboutPageComponent,HomePageComponent,ContactPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
