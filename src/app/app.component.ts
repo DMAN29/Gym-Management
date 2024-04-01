456e7e0f960c9f80f900b497e9d70e23e5386bfc
DMAN <ranjan.dr29@gmail.com>
2024-04-01 11:54:16 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
+import { ReviewPageComponent } from './review-page/review-page.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,AboutPageComponent,HomePageComponent,ContactPageComponent],
+  imports: [RouterOutlet,NavbarComponent,AboutPageComponent,HomePageComponent,ContactPageComponent,ReviewPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
