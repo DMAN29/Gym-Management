5ac92851aba3a6dd0b7f7e577bab686b0aa13adb
DMAN <ranjan.dr29@gmail.com>
2024-04-03 10:26:58 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { ReviewPageComponent } from './review-page/review-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
-import { ReviewPageComponent } from './review-page/review-page.component';
-
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,NavbarComponent,AboutPageComponent,HomePageComponent,ContactPageComponent,ReviewPageComponent],
+  imports:[HomePageComponent,NavbarComponent,ReviewPageComponent,AboutPageComponent,ContactPageComponent,FooterComponent],
+  standalone:true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'gym-management';
 }
+
+export default AppComponent; // Export the AppComponent class
